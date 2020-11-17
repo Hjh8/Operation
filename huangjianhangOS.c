b1c7493b0c5e0970db94c8d04b8f17c3b5544b4a
Hjh8 <50100494+Hjh8@users.noreply.github.com>
2020-11-17 08:16:44 +0800

    加注释

--- huangjianhangOS.c
+++ huangjianhangOS.c
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+// 标记：是否属于命令
 int flag = 0;
 
 void print(string path);
@@ -19,6 +20,7 @@ int main(){
 	string str;
 	while(1){
 		char t[1024];
+		// 仿shell输出
 		print(getcwd(t, 1024));
 
 		getline(cin, str);
@@ -27,6 +29,7 @@ int main(){
 		if(str == "mylist") mylist();
 		if(str == "mypwd") mypwd();
 		if(str.find("mycd") == 0) mycd(str);
+		
 		if(!flag) cout<<"no "<<str<<" command!"<<endl;
 	}
 
@@ -40,13 +43,12 @@ void print(string path){
 void mylist(){
 	DIR *dp = NULL;
 	struct dirent *dt = NULL;
-
+	// 打开当前目录的文件夹
 	dp = opendir("./");
 	while(1){
 		dt = readdir(dp);
 
 		if(dt == NULL) break;
-
 		if(dt->d_name[0] != '.'){
 			cout<<dt->d_name<<endl;
 		}
@@ -58,6 +60,7 @@ void mylist(){
 
 void mypwd(){
 	char buffer[1024];
+	// 获取当前路径
 	getcwd(buffer, 1024);
 	cout<<buffer<<endl;
 	flag = 1;
@@ -65,10 +68,13 @@ void mypwd(){
 
 void mycd(string s){
 	char buf[999], chPath[999];
+	// 将字符串变成字符数组
 	strcpy(buf, s.c_str());
+	// 提取路径
 	for(int i=5; i<s.length(); i++){
 		chPath[i-5] = buf[i];
 	}
+	// 改变路径
 	chdir(chPath);
 	flag = 1;
 }
