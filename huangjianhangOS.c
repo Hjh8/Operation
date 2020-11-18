89342a02ff2ecf9af0874792099d47032ba01227
codekiang <1476429595@qq.com>
2020-11-18 19:34:20 +0800

    rmdir funtion completed!

--- huangjianhangOS.c
+++ huangjianhangOS.c
@@ -15,11 +15,11 @@ void mylist();
 void mypwd();
 void mycd(string s);
 void mymkdir(string s);
-void myrmdir(string s);
+void myrmdir(char* p, int PIndex, int iteration);
 
 int main(){
-	string str;
 	while(1){
+		string str;
 		flag = 0;
 		char t[9999];
 		// 仿shell输出
@@ -32,7 +32,15 @@ int main(){
 		else if(str == "mypwd") mypwd();
 		else if(str.find("mycd") == 0) mycd(str);
 		else if(str.find("mymkdir") == 0) mymkdir(str.erase(0,7));
-		else if(str.find("myrmdir") == 0) myrmdir(str.erase(0,7));
+		else if(str.find("myrmdir") == 0) {
+			char p[999];
+			int len, PIndex, index;
+			string s = str.erase(0,7);
+			index = s.find_last_of(' ');
+			PIndex = s.find(" -p ");
+			strncpy(p, s.substr(index+1).c_str(), 999);
+			myrmdir(p, PIndex, 0);
+		}
 
 		if(!flag) cout<<"ERROR: no "<<str<<" command!"<<endl;
 	}
@@ -70,17 +78,19 @@ void mypwd(){
 }
 
 void mycd(string s){
-	int spaceNum = 0;
+	int spaceNum = 0, i;
 	char buf[999], chPath[999];
 	// 将字符串变成字符数组
 	strcpy(buf, s.c_str());
 	// 提取路径
-	for(int i=5; i<s.length(); i++){
+	for(i=4; i<s.length(); i++){
 		if(buf[i] != ' '){
-			chPath[i-5-spaceNum] = buf[i];
+			chPath[i-4-spaceNum] = buf[i];
 		}
 		else spaceNum++;
 	}
+	chPath[i-4-spaceNum] = '\0';
+
 	// 改变路径
 	if(chdir(chPath) == -1){
 		cout<<"ERROR: PATH "<<chPath<<" no exit!"<<endl;
@@ -100,7 +110,7 @@ void mymkdir(string s){
 	for(int i=0; i<len; i++){
 		if(p[i] == '/'){
 			p[i] = '\0';
-			if(access(p, 0) != 0 && PIndex>=0 && PIndex<10){
+			if(access(p, F_OK) != 0 && PIndex>=0 && PIndex<10){
 				mkdir(p, 0777);
 			}
 			else{
@@ -111,47 +121,36 @@ void mymkdir(string s){
 			p[i] = '/';
 		}
 	}
-	if(len > 0 && access(p, 0) != 0){
+	if(len > 0 && access(p, F_OK) != 0){
 		mkdir(p, 0777);
 	}
 	flag = 1;
 }
 
-void myrmdir(string s){
-	char p[999];
-	int len, PIndex, index;
-	index = s.find_last_of(' ');
-	PIndex = s.find(" -p ");
-
-	strncpy(p, s.substr(index+1).c_str(), 999);
-	len = strlen(p);
-
-	struct _stat fileStat;
-
-	if(access(p, 0) == 0 && stat(p, &fileStat) == 0 && fileStat.st_mode && _S_IFDIR){
-		if(PIndex>=0 && PIndex<10){
-
+void myrmdir(char* p, int PIndex, int iteration){
+	if(access(p, F_OK) == 0){
+		struct dirent *dt;
+		dt = readdir(opendir(p));
+		// exist dir or file
+		if(strcmp(dt->d_name, "..")){
+			if(iteration == 0) cout<< p <<" isn't empty! "<<endl;
 		}
-		else rmdir(p);
+		else{
+			rmdir(p);
+			if(PIndex<10 && PIndex>=0){
+				int len = strlen(p);
+				for(int i=len-1; i>=0; i--){
+					if(p[i] == '/'){
+						p[i] = '\0';
+						myrmdir(p, PIndex, iteration+1);
+					}
+				}
+			}
+			
+		}
 	}
 	else{
 		cout<< p <<" no exit! "<<endl;
-		flag = 1;
-		return;
-	}
-	for(int i=0; i<len; i++){
-		if(p[i] == '/'){
-			p[i] = '\0';
-			if(access(p, 0) != 0 && PIndex>=0 && PIndex<10){
-				mkdir(p, 0777);
-			}
-			else{
-				cout<< p <<" no exit! "<<endl;
-				flag = 1;
-				return;
-			}
-			p[i] = '/';
-		}
 	}
 	flag = 1;
 }
