a490af79ed27b40a911b0c4c8ad4953f3fc22cea
Hjh8 <50100494+Hjh8@users.noreply.github.com>
2020-11-20 09:26:06 +0800

    finish

--- huangjianhangOS.c
+++ huangjianhangOS.c
@@ -5,10 +5,13 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-using namespace std;
+/*
+	@Author：codekiang
+	@Time：2020.11.20
+	实现了 ls、pwd、cd、mkdir、rmdir 指令
+*/
 
-// 标记：是否属于命令
-int flag;
+using namespace std;
 
 void print(string path);
 void mylist();
@@ -16,6 +19,9 @@ void mypwd();
 void mycd(string s);
 void mymkdir(string s);
 void myrmdir(char* p, int PIndex, int iteration);
+
+// 标记：是否属于命令
+int flag;
 
 int main(){
 	while(1){
@@ -34,6 +40,7 @@ int main(){
 		else if(str.find("mymkdir") == 0) mymkdir(str.erase(0,7));
 		else if(str.find("myrmdir") == 0) {
 			char p[999];
+			// PIndex用来判断是否存在 -p
 			int len, PIndex, index;
 			string s = str.erase(0,7);
 			index = s.find_last_of(' ');
@@ -47,10 +54,17 @@ int main(){
 	return 0;
 }
 
+/*
+	@Param string path : 当前路径
+	仿shell输出
+*/
 void print(string path){
 	cout<<"user@ubuntu:"<<path<<"$ ";
 }
 
+/*
+	输出当前文件夹的所有文件
+*/
 void mylist(){
 	DIR *dp = NULL;
 	struct dirent *dt = NULL;
@@ -64,19 +78,24 @@ void mylist(){
 			cout<<dt->d_name<<endl;
 		}
 	}
-
 	closedir(dp);
 	flag = 1;
 }
 
+/*
+	获取当前路径并输出
+*/
 void mypwd(){
 	char buffer[1024];
-	// 获取当前路径
 	getcwd(buffer, 1024);
 	cout<<buffer<<endl;
 	flag = 1;
 }
 
+/*
+	@Param string s : 要改变的目录路径
+	改变当前工作路径
+*/
 void mycd(string s){
 	int spaceNum = 0, i;
 	char buf[999], chPath[999];
@@ -98,18 +117,24 @@ void mycd(string s){
 	flag = 1;
 }
 
+/*
+	@Param string s : 要创建的目录路径
+	创建目录
+*/
 void mymkdir(string s){
 	char p[999];
 	int len, PIndex, index;
 	index = s.find_last_of(' ');
 	PIndex = s.find(" -p ");
-
+	// 将字符数组变成字符串
 	strncpy(p, s.substr(index+1).c_str(), 999);
 	len = strlen(p);
 
 	for(int i=0; i<len; i++){
+		// 逐目录创建
 		if(p[i] == '/'){
 			p[i] = '\0';
+			// 目录不存在 并且 存在 -p 就创建
 			if(access(p, F_OK) != 0 && PIndex>=0 && PIndex<10){
 				mkdir(p, 0777);
 			}
@@ -121,22 +146,30 @@ void mymkdir(string s){
 			p[i] = '/';
 		}
 	}
+	// 创建最里面的文件夹
 	if(len > 0 && access(p, F_OK) != 0){
 		mkdir(p, 0777);
 	}
 	flag = 1;
 }
 
+/*
+	@Param char* p : 文件路径或文件名
+	@Param int PIndex : -p 的位置
+	@Param int iteration : 该函数循环调用的次数，防止-p循环删除时输出 isn't empty!
+*/
 void myrmdir(char* p, int PIndex, int iteration){
+	// 文件夹是否存在
 	if(access(p, F_OK) == 0){
 		struct dirent *dt;
 		dt = readdir(opendir(p));
-		// exist dir or file
+		// 一开始与".."相等表示文件夹为空
 		if(strcmp(dt->d_name, "..")){
 			if(iteration == 0) cout<< p <<" isn't empty! "<<endl;
 		}
 		else{
 			rmdir(p);
+			// 如果存在-p 则循环删除空目录
 			if(PIndex<10 && PIndex>=0){
 				int len = strlen(p);
 				for(int i=len-1; i>=0; i--){
